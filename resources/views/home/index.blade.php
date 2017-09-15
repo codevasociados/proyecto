5691eee9624c8bf8d1dffb8b2b059428907e8880
Jose Carlos <josecarl777@gmail.com>
2017-09-15 16:04:33 -0400

    cambio de css

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -8,78 +8,9 @@
 {!! Html::style('bower_components/bootstrap/css/bootstrap.min.css') !!}
 {!! Html::style('bower_components/bootstrap-material-design/dist/css/bootstrap-material-design.min.css') !!}
 {!! Html::style('bower_components/bootstrap-material-design/dist/css/ripples.min.css') !!}
+{!! Html::style('css/style_layout.css') !!}
 	@yield('css')
-	<style media="screen">
-	*{
-		margin:0px;
-		padding: 0px;
-		font-family: sans-serif;
-		color: #fff;
-	}
-	#slidebar{
-		position: fixed;
-		width: 1000px;
-		height: 100%;
-		background-color: #990000;
-		left: -1000px;
-		transition: all 500ms linear;
-	}
-	#slidebar.active{
-		left: 0px;
-
-	}
-
-	#slidebar ul li{
-		color: rgb(230,230,230,0.9);
-		list-style: none;
-		padding:10px 10px;
-		width: 90%;
-		height:55px;
-		border-bottom: 1px solid #990000;
-		overflow: hidden;
-		border-bottom: 1px solid rgba(100,100,100,0.3);
-
-	}
-	div.barra{
-		position: absolute;
-		height: 35px;
-		width: 0px;
-		background-color: #fff;
-		opacity: .4;
-		-webkit-transition: width .2s;
-	}
-
-	li:hover div.barra{
-		width:950px;
-		cursor: pointer
-	}
-
-	p.menu{
-		position: relative;
-		padding:10px;
-		padding-left: : 25px;
-		float:left;
-		color:white;
-		/*opacity: .6;*/
-		font-size: 1.0em;
-		-webkit-transition: opacity .2s;
-	}
-	#slidebar .toggle-btn{
-		position: absolute;
-		left: 1030px;
-		top: 20px;
-		cursor: pointer;
-	}
-	#slidebar .toggle-btn span{
-		display: block;
-		width: 30px;
-		height: 5px;
-		background: #151719;
-		margin: 5px 0px;
-	}
-	</style>
 </head>
-
 <body>
 
 	<div id="slidebar">
@@ -89,6 +20,7 @@
 			<span></span>
 			<span></span>
 		</div>
+		<img src="..." alt="..." class="img-circle">
 		<ul>
 			<li>
 				<div class="barra"></div>
